8aba17cf5c9450747cb81c0f84b78ff9c6526d18
Christopher Dembia <cld72@cornell.edu>
2017-02-23 13:39:56 -0800

    Add TaskScaleSetup.

--- osimpipeline/study.py
+++ osimpipeline/study.py
@@ -1,12 +1,17 @@
+import os
+
 import yaml
 
 class Trial(object):
     def __init__(self, condition, num, metadata=None):
         self.condition = condition
         self.subject = condition.subject
+        self.study = self.subject.study
         self.num = num
         self.name = 'trial%02i' % num
+        self.rel_path = os.path.join(condition.rel_path, self.name)
         def list_condition_names():
+            """Iterate through all conditions under which this trial sits."""
             cond_names = list()
             cur_cond = condition
             while cur_cond != None:
@@ -16,22 +21,42 @@ class Trial(object):
         self.id = '_'.join([self.subject.name] + list_condition_names() +
                 [self.name])
 
+class OvergroundTrial(Trial):
+    """Overground trials have their own expdata folder within the trial
+    folder."""
+    def __init__(self, condition, num, metadata=None):
+        super(OvergroundTrial, self).__init__(condition, num,
+                metadata=metadata)
+        self.marker_trajectories_fpath = os.path.join(
+                self.study.config['results_path'], 'experiments',
+                self.rel_path, 'expdata', 'marker_trajectories.trc')
+                
 class TreadmillTrial(Trial):
+    """Treadmill trials have a single expdata folder for all trials, within the
+    parent conditions folder."""
     def __init__(self, condition, num, start_time, end_time, metadata=None):
         super(TreadmillTrial, self).__init__(condition, num, metadata=metadata)
-        # TODO
+        self.marker_trajectories_fpath = os.path.join(
+                self.study.config['results_path'], 'experiments',
+                condition.rel_path, 'expdata', 'marker_trajectories.trc')
 
 class Condition(object):
-    """There can be multiple tiers of conditions."""
+    """There can be multiple tiers of conditions; conditions can be nested
+    within conditions."""
     def __init__(self, subject, parent_condition, name, metadata=None):
         """Users do not call this constructor; instead, use
         `Subject.add_condition()` or `Condition.add_condition()`. The first two
         arguments are provided internally by `add_condition()`.
         """
         self.subject = subject
+        self.study = subject.study
         self.parent_condition = parent_condition
+        # If this condition is within another condition, then that condition is
+        # the parent to this object. Otherwise, the subject is the parent.
+        self.parent = parent_condition if parent_condition != None else subject
         self.name = name
         self.metadata = metadata
+        self.rel_path = os.path.join(self.parent.rel_path, name)
         self.conditions = list()
         self.trials = list()
     def add_condition(self, *args, **kwargs):
@@ -47,8 +72,28 @@ class Condition(object):
         return None
     def contains_condition(self, name):
         return (self.get_condition(name) != None)
+    def add_overground_trial(self, *args, **kwargs):
+        """
+        Examples
+        --------
+        
+        ```
+        new_trial = cond.add_overground_trial(1)
+        ```
+        """
+        trial = OvergroundTrial(self, *args, **kwargs)
+        assert not self.contains_trial(trial.num)
+        self.trials.append(trial)
+        return trial
     def add_treadmill_trial(self, *args, **kwargs):
-        """Example: `new_trial = cond.add_trial(1, start_time, end_time)` """
+        """
+        Examples
+        --------
+        
+        ```
+        new_trial = cond.add_treadmill_trial(1, start_time, end_time)
+        ```
+        """
         trial = TreadmillTrial(self, *args, **kwargs)
         assert not self.contains_trial(trial.num)
         self.trials.append(trial)
@@ -68,6 +113,9 @@ class Subject(object):
         self.name = 'subject%02i' % num
         self.mass = mass
         self.metadata = metadata
+        # Relative path to the subject folder; can be used for the source
+        # directory or the results directory.
+        self.rel_path = self.name
         self.conditions = list()
         self.tasks = list()
     def add_condition(self, *args, **kwargs):
--- osimpipeline/vital_tasks.py
+++ osimpipeline/vital_tasks.py
@@ -14,7 +14,8 @@ class TaskCopyMotionCaptureData(task.SubjectTask):
     treadmill trials) that contains `marker_trajectories.trc` and
     `ground_reaction.mot`.
     
-    Task name: `subject<num>_copy_data`"""
+    Task name: `subject<num>_copy_data`
+    """
     # TODO Turn into a StudyTask, since there is very little about this task
     # that is subject-specific.
     REGISTRY = [] # TODO Find a way to make this unnecessary.
@@ -92,6 +93,177 @@ class TaskCopyMotionCaptureData(task.SubjectTask):
             import shutil
             shutil.copyfile(source, destination)
 
+
+class TaskScaleSetup(task.SubjectTask):
+    """Create a setup file for the OpenSim Scale tool. You must place a
+    template model markerset located at
+    `templates/scale/prescale_markerset.xml`. This task creates a copy of this
+    file for each subject, since you may need to tweak the markerset for
+    individual subjects. The fields @STUDYNAME@ and @SUBJECTNAME@ in the
+    template will be replaced by the correct values.
+    
+    Task name: `subject<num>_scale_setup`
+    """
+    REGISTRY = []
+    def __init__(self, subject, init_time, final_time,
+            mocap_trial, edit_setup_function, addtl_file_dep=[]):
+        """
+        Parameters
+        ----------
+        init_time : float
+            The initial time from the motion capture trial at which to start
+            averaging the marker frames.
+        final_time : float
+            The final time from the motion capture trial at which to stop
+            averaging the marker frames.
+        mocap_trial : study.Trial
+            The Trial whose marker trajectories to use for both the model
+            scaling and marker placer steps. This is ususally a static trial
+            but it does not need to be.
+        addtl_file_dep : list of str
+            Any other files that should be added as file dependencies, changes
+            to which would make this task out of date. Usually, you might
+            specify the dodo file and whichever file contains the
+            `edit_setup_function`.
+
+        """
+        super(TaskScaleSetup, self).__init__(subject)
+        self.subj_mass = self.subject.mass
+        self.init_time = init_time
+        self.final_time = final_time
+        self.mocap_trial = mocap_trial
+        self.name = '%s_scale_setup' % (self.subject.name)
+        self.doc = "Create a setup file for OpenSim's Scale Tool."
+        self.edit_setup_function = edit_setup_function
+        self.results_scale_path = os.path.join(
+                self.study.config['results_path'], 'experiments',
+                self.subject.rel_path, 'scale')
+
+        self.add_action(
+                {'marker_traj':
+                    self.mocap_trial.marker_trajectories_fpath,
+                 'generic_model': self.study.generic_model_fpath,
+                    },
+                {'setup': os.path.join(self.results_scale_path, 'setup.xml')
+                    },
+                self.create_scale_setup)
+
+        # MarkerSet for the Scale Tool.
+        self.source_scale_path = os.path.join(self.subject.rel_path, 'scale')
+        self.prescale_template_fpath = 'templates/scale/prescale_markerset.xml'
+        self.prescale_fpath = os.path.join(self.source_scale_path, 
+                '%s_prescale_markerset.xml' % self.subject.name)
+        if not os.path.exists(self.prescale_fpath):
+            # The user does not yet have a markerset in place; fill out the
+            # template.
+            self.add_action(
+                    {'template': self.prescale_template_fpath},
+                    {'subjspecific': self.prescale_fpath},
+                    self.fill_prescale_markerset_template)
+        else:
+            # We have already filled out the template prescale markerset,
+            # and the user might have made changes to it.
+            self.file_dep.append(self.prescale_fpath)
+
+        # TODO self.actions += [self.prescale_markerset_template]
+
+        #self.prescale_rel_fpath = ('subject%02i_prescale_markerset.xml' %
+        #        self.subj)
+        #self.prescale_fpath = join(self.subj_str, self.prescale_rel_fpath)
+
+        #if not os.path.exists(self.prescale_fpath):
+        #    self.file_dep += [self.prescale_template_fpath]
+        #    self.targets += [self.prescale_fpath]
+
+        #else:
+        #    self.file_dep += [self.prescale_fpath]
+
+        self.file_dep += addtl_file_dep
+
+    def fill_prescale_markerset_template(self, file_dep, target):
+        # TODO
+        if not os.path.exists(target['subjspecific']):
+            ft = open(file_dep['template'])
+            content = ft.read()
+            content = content.replace('@STUDYNAME@', self.study.name)
+            content = content.replace('@SUBJECTNAME@', self.subject.name)
+            ft.close()
+            if not os.path.exists(self.source_scale_path):
+                os.makedirs(self.source_scale_path)
+            f = open(target['subjspecific'], 'w')
+            f.write(content)
+            f.close()
+
+    def create_scale_setup(self, file_dep, target):
+        import opensim as osm
+        from perimysium import modeling as pmm
+
+        # EDIT THESE FIELDS IN PARTICULAR.
+        # --------------------------------
+        time_range = osm.ArrayDouble()
+        time_range.append(self.init_time)
+        time_range.append(self.final_time)
+
+        tool = osm.ScaleTool()
+        tool.setName('%s_%s' % (self.study.name, self.subject.name))
+        tool.setSubjectMass(self.subject.mass)
+
+        # GenericModelMaker
+        # =================
+        gmm = tool.getGenericModelMaker()
+        gmm.setModelFileName(os.path.relpath(file_dep['generic_model'],
+            self.results_scale_path))
+        gmm.setMarkerSetFileName(os.path.relpath(self.prescale_fpath,
+            self.results_scale_path))
+
+        # ModelScaler
+        # ===========
+        scaler = tool.getModelScaler()
+        scaler.setPreserveMassDist(True)
+        marker_traj_rel_fpath = os.path.relpath(file_dep['marker_traj'],
+            self.results_scale_path)
+        scaler.setMarkerFileName(marker_traj_rel_fpath)
+
+        scale_order_str = osm.ArrayStr()
+        scale_order_str.append('manualScale')
+        scale_order_str.append('measurements')
+        scaler.setScalingOrder(scale_order_str)
+
+        scaler.setTimeRange(time_range)
+
+        mset = scaler.getMeasurementSet()
+
+        # Manual scalings
+        # ---------------
+        sset = scaler.getScaleSet()
+
+        # MarkerPlacer
+        # ============
+        placer = tool.getMarkerPlacer()
+        placer.setStaticPoseFileName(marker_traj_rel_fpath)
+        placer.setTimeRange(time_range)
+        placer.setOutputModelFileName('%s.osim' % (self.subject.name))
+        placer.setOutputMotionFileName(
+                '%s_%s_staticpose.mot' % (self.study.name, self.subject.name))
+        placer.setOutputMarkerFileName(
+                '%s_%s_markerset.xml' % (self.study.name, self.subject.name))
+        ikts = pmm.IKTaskSet(placer.getIKTaskSet())
+
+        self.edit_setup_function(pmm, mset, sset, ikts)
+
+        # Validate Scales
+        # ===============
+        model = osm.Model(file_dep['generic_model'])
+        bset = model.getBodySet()
+        for iscale in range(sset.getSize()):
+            segment_name = sset.get(iscale).getSegmentName()
+            if not bset.contains(segment_name):
+                raise Exception("You specified a Scale for "
+                        "body %s but it's not in the model." % segment_name)
+
+        if not os.path.exists(self.results_scale_path):
+            os.makedirs(self.results_scale_path)
+        tool.printToXML(target['setup'])
             
 
 
